62607234cbf06c99cee70552fdfd802c393eaf66
david <litbigo@gmail.com>
2018-10-20 14:01:30 +0200

    #commit 4

---
+++ emorobots/src/test/java/com/res/SubjectObserverTest.java
@@ -0,0 +1,11 @@
+package com.res;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({})
+public class SubjectObserverTest {
+
+}
