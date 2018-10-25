44184015fc5aadbd52dcbc153554641d46fd7f93
david <litbigo@gmail.com>
2018-10-25 02:35:43 +0200

    #commit 16 Buggy!

---
+++ emorobots-parent/src/main/java/com/res/emorobot/App.java
@@ -0,0 +1,13 @@
+package com.res.emorobot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
